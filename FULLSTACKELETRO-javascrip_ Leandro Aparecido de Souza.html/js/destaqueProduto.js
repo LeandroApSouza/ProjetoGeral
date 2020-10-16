14dc6cb4ee460419eeca8a4651070d5ade31c70d
LeandoApSouza <renova.servicosind@gmail.com>
2020-10-16 16:14:23 -0300

    todos por seguranca

---
+++ FULLSTACKELETRO-javascrip_ Leandro Aparecido de Souza.html/js/destaqueProduto.js
@@ -0,0 +1,209 @@
+    // função onmouseover GELADEIRAS//
+function grande1(){
+    document.getElementById("lupa1").width=320;
+    }
+function normal1(){
+    document.getElementById("lupa1").width=120;
+    }
+        
+function grande2(){
+   document.getElementById("lupa2").width=320;
+    }
+function normal2(){
+  document.getElementById("lupa2").width=120;
+    }
+
+function grande3(){
+    document.getElementById("lupa3").width=320;
+    }
+function normal3(){
+    document.getElementById("lupa3").width=120;
+    }
+        
+function grande4(){
+    document.getElementById("lupa4").width=320;
+    }
+function normal4(){
+    document.getElementById("lupa4").width=120;
+    }
+
+    // função onmouseover COOKTOOPS//
+    function grandeCt1(){
+        document.getElementById("lupaCt1").width=320;
+        }
+    function normalCt1(){
+        document.getElementById("lupaCt1").width=120;
+        }
+            
+    function grandeCt2(){
+       document.getElementById("lupaCt2").width=320;
+        }
+    function normalCt2(){
+      document.getElementById("lupaCt2").width=120;
+        }
+    
+    function grandeCt3(){
+        document.getElementById("lupaCt3").width=320;
+        }
+    function normalCt3(){
+        document.getElementById("lupaCt3").width=120;
+        }
+            
+    function grandeCt4(){
+        document.getElementById("lupaCt4").width=320;
+        }
+    function normalCt4(){
+        document.getElementById("lupaCt4").width=120;
+        }
+    
+        
+            // função onmouseover LAVADOURAS//
+function grandeLv1(){
+    document.getElementById("lupaLv1").width=320;
+    }
+function normalLv1(){
+    document.getElementById("lupaLv1").width=120;
+    }
+        
+function grandeLv2(){
+   document.getElementById("lupaLv2").width=320;
+    }
+function normalLv2(){
+  document.getElementById("lupaLv2").width=120;
+    }
+
+function grandeLv3(){
+    document.getElementById("lupaLv3").width=320;
+    }
+function normalLv3(){
+    document.getElementById("lupaLv3").width=120;
+    }
+        
+function grandeLv4(){
+    document.getElementById("lupaLv4").width=320;
+    }
+function normalLv4(){
+    document.getElementById("lupaLv4").width=120;
+    }
+    
+
+        // função onmouseover TELEVISORES//
+function grandeTv1(){
+    document.getElementById("lupaTv1").width=320;
+    }
+function normalTv1(){
+    document.getElementById("lupaTv1").width=120;
+    }
+        
+function grandeTv2(){
+   document.getElementById("lupaTv2").width=320;
+    }
+function normalTv2(){
+  document.getElementById("lupaTv2").width=120;
+    }
+
+function grandeTv3(){
+    document.getElementById("lupaTv3").width=320;
+    }
+function normalTv3(){
+    document.getElementById("lupaTv3").width=120;
+    }
+        
+function grandeTv4(){
+    document.getElementById("lupaTv4").width=320;
+    }
+function normalTv4(){
+    document.getElementById("lupaTv4").width=120;
+    }
+    
+
+        // função onmouseover LIQUITFICAODRES//
+function grandeLf1(){
+    document.getElementById("lupaLf1").width=320;
+    }
+function normalLf1(){
+    document.getElementById("lupaLf1").width=120;
+    }
+        
+function grandeLf2(){
+   document.getElementById("lupaLf2").width=320;
+    }
+function normalLf2(){
+  document.getElementById("lupaLf2").width=120;
+    }
+
+function grandeLf3(){
+    document.getElementById("lupaLf3").width=320;
+    }
+function normalLf3(){
+    document.getElementById("lupaLf3").width=120;
+    }
+        
+function grandeLf4(){
+    document.getElementById("lupaLf4").width=320;
+    }
+function normalLf4(){
+    document.getElementById("lupaLf4").width=120;
+    }
+    
+
+        // função onmouseover FOGOES//
+function grandeFg1(){
+    document.getElementById("lupaFg1").width=320;
+    }
+function normalFg1(){
+    document.getElementById("lupaFg1").width=120;
+    }
+        
+function grandeFg2(){
+   document.getElementById("lupaFg2").width=320;
+    }
+function normalFg2(){
+  document.getElementById("lupaFg2").width=120;
+    }
+
+function grandeFg3(){
+    document.getElementById("lupaFg3").width=320;
+    }
+function normalFg3(){
+    document.getElementById("lupaFg3").width=120;
+    }
+        
+function grandeFg4(){
+    document.getElementById("lupaFg4").width=320;
+    }
+function normalFg4(){
+    document.getElementById("lupaFg4").width=120;
+    }
+    
+
+        // função onmouseover MICROONDAS//
+function grandeMo1(){
+    document.getElementById("lupaMo1").width=320;
+    }
+function normalMo1(){
+    document.getElementById("lupaMo1").width=120;
+    }
+        
+function grandeMo2(){
+   document.getElementById("lupaMo2").width=320;
+    }
+function normalMo2(){
+  document.getElementById("lupaMo2").width=120;
+    }
+
+function grandeMo3(){
+    document.getElementById("lupaMo3").width=320;
+    }
+function normalMo3(){
+    document.getElementById("lupaMo3").width=120;
+    }
+        
+function grandeMo4(){
+    document.getElementById("lupaMo4").width=320;
+    }
+function normalMo4(){
+    document.getElementById("lupaMo4").width=120;
+    }
+    
+
---
+++ FULLSTACKELETRO-javascrip_ Leandro Aparecido de Souza.html/js/ofertas.js
@@ -0,0 +1,106 @@
+
+// função onclick para GELAREIRAS//
+
+function novoG1(){ 
+       document.getElementById("g1").innerHTML="R$ 5.019,00";
+     }
+function novoG2(){           
+          document.getElementById("g2").innerHTML="R$ 3.219,00";
+     }
+function novoG3(){ 
+     document.getElementById("g3").innerHTML="R$ 3.999,00";
+     }
+function novoG4(){           
+          document.getElementById("g4").innerHTML="R$ 4.099,00";
+     }
+
+     // função onclick para COOKTOOPS//
+
+function novoCT1(){ 
+     document.getElementById("ct1").innerHTML="R$ 699,00";
+   }
+function novoCT2(){           
+        document.getElementById("ct2").innerHTML="R$ 819,00";
+   }
+function novoCT3(){ 
+   document.getElementById("ct3").innerHTML="R$ 639,00";
+   }
+function novoCT4(){           
+        document.getElementById("ct4").innerHTML="R$ 599,00";
+   }
+
+     // função onclick para LAVADOURAS//
+
+   function novoLv1(){ 
+     document.getElementById("Lv1").innerHTML="R$ 1.499,00";
+   }
+function novoLv2(){           
+        document.getElementById("Lv2").innerHTML="R$ 1.199,00";
+   }
+function novoLv3(){ 
+   document.getElementById("Lv3").innerHTML="R$ 1.119,00";
+   }
+function novoLv4(){           
+        document.getElementById("Lv4").innerHTML="R$ 1.099,00";
+   }
+
+
+   // função onclick para TELEVISORES//
+
+function novoTv1(){ 
+     document.getElementById("Tv1").innerHTML="R$ 3.999,00";
+   }
+function novoTv2(){           
+        document.getElementById("Tv2").innerHTML="R$ 3.399,00";
+   }
+function novoTv3(){ 
+   document.getElementById("Tv3").innerHTML="R$ 2.099,00";
+   }
+function novoTv4(){           
+        document.getElementById("Tv4").innerHTML="R$ 1.199,00";
+   }
+
+   // função onclick para LIGUITIFICADORES//
+
+function novoLf1(){ 
+     document.getElementById("Lf1").innerHTML="R$ 119,00";
+   }
+function novoLf2(){           
+        document.getElementById("Lf2").innerHTML="R$ 129,00";
+   }
+function novoLf3(){ 
+   document.getElementById("Lf3").innerHTML="R$ 100,00";
+   }
+function novoLf4(){           
+        document.getElementById("Lf4").innerHTML="R$ 189,00";
+   }
+
+   // função onclick para FOGÕES//
+
+function novoFo1(){ 
+     document.getElementById("Fo1").innerHTML="R$ 1.139,00";
+   }
+function novoFo2(){           
+        document.getElementById("Fo2").innerHTML="R$ 1.009,00";
+   }
+function novoFo3(){ 
+   document.getElementById("Fo3").innerHTML="R$ 1.299,00";
+   }
+function novoFo4(){           
+        document.getElementById("Fo4").innerHTML="R$ 979,00";
+   }
+
+   // função onclick para MICROONDAS//
+
+function novoMo1(){ 
+     document.getElementById("Mo1").innerHTML="R$ 499,00";
+   }
+function novoMo2(){           
+        document.getElementById("Mo2").innerHTML="R$ 589,00";
+   }
+function novoMo3(){ 
+   document.getElementById("Mo3").innerHTML="R$ 589,00";
+   }
+function novoMo4(){           
+        document.getElementById("Mo4").innerHTML="R$ 399,00";
+   }
